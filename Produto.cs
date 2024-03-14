a5dac369c09a512e465f2f509aadd353f5499efc
Charleu Limeira <sdlimeira0@gmail.com>
2024-03-14 13:38:52 -0300

    Desconto produto

---
+++ Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+    class Produto
+    {
+        public string Nome;
+        public double PrecoUnitario;
+        public int Quantidade;
+
+        public Produto(string nome, double precoUnitario, int quantidade)
+        {
+            Nome = nome;
+            PrecoUnitario = precoUnitario;
+            Quantidade = quantidade;
+        }
+
+        public double PrecoTotal => CalcularPrecoTotal(PrecoUnitario, Quantidade);
+        public double Desconto => CalcularDesconto(Quantidade);
+
+        public static double CalcularPrecoTotal(double preco, int quantidade)
+        {
+            double desconto = CalcularDesconto(quantidade);
+            return preco * quantidade * (1 - desconto);
+        }
+
+        public static double CalcularDesconto(int quantidade)
+        {
+            if (quantidade > 50)
+                return 0.25;
+            else if (quantidade > 20)
+                return 0.2;
+            else if (quantidade > 10)
+                return 0.1;
+            else
+                return 0;
+        }
+    }
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DescontoProdutoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = new string('-', 40);
+            Console.WriteLine("Informe o nome do produto:");
+            string nomeProduto = Console.ReadLine();
+            Console.WriteLine("Informe o preço do produto:");
+            double precoProduto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a quantidade do produto:");
+            int quantidadeProduto = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nomeProduto, precoProduto, quantidadeProduto);
+
+            Console.WriteLine(sep);
+            Console.WriteLine($"Valor total: {produto.PrecoTotal.ToString("C")}");
+            Console.WriteLine(sep);
+            Console.WriteLine($"Desconto: {produto.Desconto.ToString("C")}");
+            Console.WriteLine(sep);
+        }
+    }
+}
